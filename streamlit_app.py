bb6ffb57aebb225c8969029bf7efcf389e37dcd3
Hubert Owusu <hubert7856@gmail.com>
2024-08-11 23:55:53 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import pandas as pd
 st.title('College Application Support')
 
 # Keyword/hastags, tweet count and date range(start and end)
-Hashtag = st.sidebar.text_input("Enter the Hashtag or Keyword of Tweets : ")
+Hashtag = st.sidebar.text_input("Enter the Hashtag or Keyword of Tweets")
 No_of_tweets = st.sidebar.slider("Number of Tweets: ", 1, 100, 10)
-start_date = st.sidebar.date_input("Start Date (YYYY-MM-DD) : ")
-end_date = st.sidebar.date_input("End Date (YYYY-MM-DD) : ")
+start_date = st.sidebar.date_input("Start Date")
+end_date = st.sidebar.date_input("End Date")
 Scraped_date = str(date.today())
