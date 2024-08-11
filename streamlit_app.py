d7cfb89b8293f837540e94e4634668868c096b06
Hubert Owusu <hubert7856@gmail.com>
2024-08-11 23:47:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
-st.title('***College Application Support***')
+st.title('*College Application Support*')
 
-st.write('Hello world!')
+# Keyword/hastags, tweet count and date range(start and end)
+Hashtag = st.sidebar.text_input("Enter the Hashtag or Keyword of Tweets : ")
+No_of_tweets = st.sidebar.number_input("Number of Tweets needed : ", min_value= 1, max_value= 1000, step= 1)
+st.sidebar.write(":green[Select the Date range]")
+start_date = st.sidebar.date_input("Start Date (YYYY-MM-DD) : ")
+end_date = st.sidebar.date_input("End Date (YYYY-MM-DD) : ")
+Scraped_date = str(date.today())
