84ed130315813ef5c70e175c3adf05d49426e68b
Hubert Owusu <hubert7856@gmail.com>
2024-08-12 15:31:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,13 +47,18 @@ end_date = st.date_input("# End Date")
 scraped_date = str(date.today())
 input_submit_button = st.button('Load tweets')
 
-# When When Input Submission Button is clicked
-if input_submit_button:
-    # Load Nitter
+# Check if the scraper is already in session state
+if 'scraper' not in st.session_state:
     with suppress_tqdm():
         st.write('App is loading...')
-        scraper = Nitter(log_level=1, skip_instance_check=False)
-    
+        st.session_state.scraper = Nitter(log_level=1, skip_instance_check=False)
+
+# Retrieve the scraper from session state
+scraper = st.session_state.scraper
+
+
+# When When Input Submission Button is clicked
+if input_submit_button:
     tweets = get_tweets(keywords, 'term', tweet_count, str(start_date), str(end_date))
     st.write(tweets)
 
