2523cc1370e543266edb2b5644283a062240438a
Hubert Owusu <hubert7856@gmail.com>
2024-08-12 13:53:51 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_tags import st_tags, st_tags_sidebar
 from datetime import date
 import pandas as pd
-from ntscraper import Nitter
+#from ntscraper import Nitter
 import sys
 #import contextlib
 
