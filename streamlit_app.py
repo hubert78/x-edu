bf47fa0275d21dfdf72f49b42e86bfa92e5ac3be
Hubert Owusu <hubert7856@gmail.com>
2024-08-12 00:43:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_tags import st_tags, st_tags_sidebar
 from datetime import date
 import pandas as pd
+from ntscraper import Nitter
 
 st.title('College Application Support')
 scraper = Nitter()
