a1a26f5d1b6c44b19ced38329932a5d4e714ebde
Hubert Owusu <hubert7856@gmail.com>
2024-08-12 00:25:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,20 @@ import pandas as pd
 st.title('College Application Support')
 
 # Keyword/hastags, tweet count and date range(start and end)
-Hashtag = st_tags_sidebar(
-  label='Add Hashtag or Keywords',
-  text='press enter to add more',
-  suggestion=['PhD', 'Fee', 'Waiver'],
-  maxtags=3,
-  key='1'
-)
+#Hashtag = st_tags_sidebar(
+#  label='Add Hashtag or Keywords',
+#  text='press enter to add more',
+#  suggestion=['PhD', 'Fee', 'Waiver'],
+#  maxtags=3,
+#  key='1'
+#)
+
+keyword = st_tags_sidebar(label='# Enter Keywords:',
+                          text='Press enter to add more',
+                          value=['Zero', 'One', 'Two'],
+                          suggestions=['five', 'six', 'seven', 'eight', 'nine', 'three', 'eleven', 'ten', 'four'],
+                          maxtags=maxtags_sidebar,
+                          key="afrfae")
 
 No_of_tweets = st.sidebar.slider("Number of Tweets: ", 1, 100, 10)
 start_date = st.sidebar.date_input("Start Date")
