4722c34fc45ee6681b7111c8cbab2a6b488a8186
Hubert Owusu <hubert7856@gmail.com>
2024-08-12 14:27:52 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,16 @@ from ntscraper import Nitter
 import sys
 import contextlib
 
-
+# Function to suppress the sys print output of ntscraper
+@contextlib.contextmanager
+def suppress_tqdm():
+    original_stdout = sys.stdout
+    sys.stdout = open('/dev/null', 'w')
+    try:
+        yield
+    finally:
+        sys.stdout = original_stdout
+      
   
 # Function to get tweets in a DataFrame
 def get_tweets(term, mode, num, since, until):
@@ -46,26 +55,16 @@ input_submit_button = st.button('Find Tweets')
 
 
 
-# Function to suppress the sys print output of ntscraper
-@contextlib.contextmanager
-def suppress_tqdm():
-    original_stdout = sys.stdout
-    sys.stdout = open('/dev/null', 'w')
-    try:
-        yield
-    finally:
-        sys.stdout = original_stdout
-        
-with suppress_tqdm():
-    scraper = Nitter(log_level=1, skip_instance_check=False)
+#with suppress_tqdm():
+#    scraper = Nitter(log_level=1, skip_instance_check=False)
 
-st.write('App about to load.')
+st.write(keywords)
 
 # When When Input Submission Button is clicked
 if input_submit_button:    
     st.write('App is loading...')
-    tweets = get_tweets(keywords, 'term', tweet_count, start_date, end_date)
-    st.write(tweets)
+    #tweets = get_tweets(keywords, 'term', tweet_count, start_date, end_date)
+    #st.write(tweets)
 
 
 
