8c4220ab85df22b26d67fd044072970e31ea38ea
Hubert Owusu <hubert7856@gmail.com>
2024-08-12 02:19:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import streamlit as st
 from streamlit_tags import st_tags, st_tags_sidebar
 from datetime import date
 import pandas as pd
-from Scweet.scweet import scrape
+from ntscraper import Nitter
+
 
 st.title('College Application Support')
-#scraper = Nitter()
+scraper = Nitter()
 
 # Keyword/hastags, tweet count and date range(start and end)
 keyword = st_tags_sidebar(label='Enter Keywords:',
@@ -21,9 +22,6 @@ Scraped_date = str(date.today())
 submitted = st.sidebar.button('Find Tweets')
  
 
-data = scrape(words=['PhD Bioinformatics'], since="2024-10-01", until="2024-10-05", from_account = None, 
-              interval=1, headless=False, display_type="Top", save_images=False, lang="en", 
-              resume=False, filter_replies=False, proximity=False)
-data
+
 
 
