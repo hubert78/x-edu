d5f64b313108b5295d516b7fe08da114dac2fbe4
Hubert Owusu <hubert7856@gmail.com>
2024-08-12 14:45:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,23 +15,26 @@ def suppress_tqdm():
         yield
     finally:
         sys.stdout = original_stdout
+
+with suppress_tqdm():
+    st.write('App is loading...')
+    scraper = Nitter(log_level=1, skip_instance_check=False)
       
   
 # Function to get tweets in a DataFrame
 def get_tweets(term, mode, num, since, until):
-    with suppress_tqdm():  
-      tweets = scraper.get_tweets(term, mode=mode, number=num, since=since, until=until)
+    tweets = scraper.get_tweets(term, mode=mode, number=num, since=since, until=until)
     
     final_tweets = []
     for tweet in tweets['tweets']:
         tweet_data = [
-            tweet['user']['username'], tweet['user']['name'], tweet['user']['avatar'],
+            tweet['user']['username'], tweet['user']['avatar'],
             tweet['link'], tweet['text'], tweet['date'], tweet['stats']['likes'],
             tweet['pictures']]
         final_tweets.append(tweet_data)
     
     columns = [
-        'username', 'user', 'avatar', 'link', 'text', 'date', 'likes', 'pictures'
+        'username', 'avatar', 'link', 'text', 'date', 'likes', 'pictures'
     ]
     
     return pd.DataFrame(final_tweets, columns=columns)
@@ -42,7 +45,7 @@ st.title('College Application Support')
 
 # Get keywords/hastags, tweet_count and date range(start and end)
 keywords = st.text_input('Enter Keywords')
-tweet_count = st.slider("# Number of Tweets: ", 1, 100, 10)
+tweet_count = st.slider("# Number of Tweets: ", 1, 20, 10)
 start_date = st.date_input("# Start Date")
 end_date = st.date_input("# End Date")
 scraped_date = str(date.today())
@@ -50,8 +53,6 @@ input_submit_button = st.button('Find Tweets')
 
 
 
-with suppress_tqdm():
-    scraper = Nitter(log_level=1, skip_instance_check=False)
 
 st.write('App is loading...')
 
