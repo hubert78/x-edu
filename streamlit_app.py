8da3c1de65db54ee5fcc4169ccf549ea41d24a20
Hubert Owusu <hubert7856@gmail.com>
2024-08-12 01:01:14 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_tags import st_tags, st_tags_sidebar
 from datetime import date
 import pandas as pd
-from ntscraper import Nitter
+from Scweet.scweet import scrape
 
 st.title('College Application Support')
 #scraper = Nitter()
@@ -19,3 +19,9 @@ start_date = st.sidebar.date_input("# Start Date")
 end_date = st.sidebar.date_input("# End Date")
 Scraped_date = str(date.today())
 submitted = st.sidebar.button('Find Tweets')
+
+
+data = scrape(words=['Bioinformatics'], since='2024-08-01', until'2024-08-9lang='en', interval=1)
+data
+
+
