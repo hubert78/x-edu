70267b20a2d224aff3ec1b105ab0a86084b99310
Hubert Owusu <hubert7856@gmail.com>
2024-08-12 00:30:30 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,12 @@ import pandas as pd
 
 st.title('College Application Support')
 
-
+# Keyword/hastags, tweet count and date range(start and end)
+hashtag = st_tags_sidebar(label='Add Hashtag or Keywords',
+                          text='press enter to add more',
+                          suggestion=['PhD', 'Fee', 'Waiver'],
+                          maxtags=3,
+                          key='1')
 
 keyword = st_tags_sidebar(label='# Enter Keywords:',
                           text='Press enter to add more',
