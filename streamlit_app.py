1a69094ca394808be07fae74b95d170f596aaebd
Hubert Owusu <hubert7856@gmail.com>
2024-08-13 20:45:37 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -208,7 +208,7 @@ if load_twt_button:
 
 # Filter and display loaded tweets based on context
 if not st.session_state.loaded_tweets.empty:
-    st.write('\n')
+    st.write('\n\n')
     unique_contexts = st.session_state.loaded_tweets['context'].unique()
     
     # Dropdown for filtering by context
