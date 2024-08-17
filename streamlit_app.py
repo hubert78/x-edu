6ffc4d4045a09ac781cf9afee40392a9586f2420
Hubert Owusu <hubert7856@gmail.com>
2024-08-17 12:46:29 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -189,11 +189,11 @@ nittered = False
 # When  Input Submission Button is clicked
 if input_submit_button:
     with st.spinner('Fetching tweets...'):
-        
         # Load Nitter
         with suppress_tqdm():
             scraper = Nitter(log_level=1, skip_instance_check=False)
-    
+            
+    with st.spinner('Checking received data...'):
         # Get tweets from Nitter
         dl_tweets = get_tweets(keywords, 'term', tweet_count, str(start_date), str(end_date), context)
         nittered = True
